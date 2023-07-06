c5cc42206dbaf7dc854f5f9effd91946af4dc4e3
lcs <lcs.shun@foxmail.com>
2023-07-07 00:33:49 +0800

    receive arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::{env};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        panic!("the arguments error, please input query and file path.")
+    }
+
+    for a in &args {
+        println!("the arg = {:?}", a);
+    }
 }
