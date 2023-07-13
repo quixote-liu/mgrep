504fb620a8fac312999444654728895bf2832039
lcs <lcs.shun@foxmail.com>
2023-07-13 23:50:52 +0800

    optimize error output and readme doc

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # mgrep
-A command line tool that like grep.
+A command line tool that like grep. you cloud use it to search message.
+
+# usage
+
+### build executable file in project root path.
+```
+cargo build --release
+```
+
+### use mgrep to query the specified message.
+the syntax:
+```
+mgrep <query> <file path>
+```
+example:
+```
+./target/release/mgrep to poem.txt
+Are you nobody, too?
+How dreary to be somebody!
+```
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@ use mgrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args)
-        .expect("Should have been able to read the file");
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     if let Err(e) = mgrep::run(config){
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
