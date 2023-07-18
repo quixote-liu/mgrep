f2d6b80ed9ab99ed7f9dd310edf79816423d4d84
lcs <lcs.shun@foxmail.com>
2023-07-19 01:00:44 +0800

    add iterator

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 use std::{fs, env};
+use std::iter::Iterator;
 
 pub struct Config {
     pub query: String,
@@ -8,16 +9,36 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build(
+        mut args: impl Iterator<Item = String>,
+    ) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config{query: query, file_path: file_path, ignore_case: ignore_case})
+        Ok(Config { query: query, file_path:file_path, ignore_case:ignore_case})
     }
+
+    // pub fn build(args: &[String]) -> Result<Config, &'static str> {
+    //     if args.len() < 3 {
+    //         return Err("not enough arguments");
+    //     }
+    //     let query = args[1].clone();
+    //     let file_path = args[2].clone();
+    //     let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+    //     Ok(Config{query: query, file_path: file_path, ignore_case: ignore_case})
+    // }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
@@ -38,13 +59,15 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    // let mut results = Vec::new();
+    // for line in contents.lines() {
+    //     if line.contains(query) {
+    //         results.push(line);
+    //     }
+    // }
+    // results
+
+    contents.lines().filter(|line| line.contains(query)).collect()
 }
 
 fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ use std::{env, process};
 use mgrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().iter();
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
