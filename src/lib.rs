e8e3fdbccd4ac762d9ff0bd40eceae91f6be946c
刘成顺 <liuchengshun@flexem.com>
2023-10-09 19:29:22 +0800

    clean code

--- src/lib.rs
+++ src/lib.rs
@@ -28,17 +28,6 @@ impl Config {
 
         Ok(Config { query: query, file_path:file_path, ignore_case:ignore_case})
     }
-
-    // pub fn build(args: &[String]) -> Result<Config, &'static str> {
-    //     if args.len() < 3 {
-    //         return Err("not enough arguments");
-    //     }
-    //     let query = args[1].clone();
-    //     let file_path = args[2].clone();
-    //     let ignore_case = env::var("IGNORE_CASE").is_ok();
-
-    //     Ok(Config{query: query, file_path: file_path, ignore_case: ignore_case})
-    // }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
@@ -59,14 +48,6 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    // let mut results = Vec::new();
-    // for line in contents.lines() {
-    //     if line.contains(query) {
-    //         results.push(line);
-    //     }
-    // }
-    // results
-
     contents.lines().filter(|line| line.contains(query)).collect()
 }
 
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ use std::{env, process};
 use mgrep::Config;
 
 fn main() {
-    // let args: Vec<String> = env::args().iter();
-
     let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
